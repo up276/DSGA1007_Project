e041298337c06f5a84dd4f0a7fea2adf96ac9909
mc3784 <mc3784@nyu.edu>
2015-12-15 12:27:00 -0500

    added comment

--- YoutubeAnalysis/DataExplorer.py
+++ YoutubeAnalysis/DataExplorer.py
@@ -14,36 +14,6 @@ class DataStatistics():
     Catagory_mapping = {2: 'Autos & Vehicles', 23: 'Comedy' , 27: 'Education', 24: 'Entertainment' , 1: 'Film & Animation', 20: 'Gaming' ,26: 'Howto & Style', 10: 'Music',25: 'News & Politics', 29:'Nonprofits & Activism' ,22:'People & Blogs', 15: 'Pets & Animals' , 28: 'Science & Technology', 17: 'Sports' , 19:'Travel & Events'}
     def __init__(self):
         print "Perform data analysiss..."
-    """
-    def printExploreOptions(self):
-        print "String with a short description of the program and how it works. Than the list of option...\n \
-               2) press 1 for general analysis \n \
-               1) press 2 for individual analyisis of each video catagory \n \
-               3) press 3 for individual feature analysis "
-
-    def InitiateDataExplorer(self,data):
-        self.data = data
-        self.printExploreOptions()
-        self.userInputLoop()
-
-    def userInputLoop(self):
-        '''
-            Create a loop asking the user which action he or she wants to take. The loop is break (and the program ends) whenever the user type quit.
-        '''
-        userInput=""
-        try:
-            while userInput != "quit":
-                userInput = raw_input("Please enter one of the number: ")
-                if userInput == "1":
-                    pass
-                elif userInput == "2":
-                    self.individual_videocatagory_analysis()
-                elif userInput == "3":
-                    self.individual_feature_analysis()
-        except KeyboardInterrupt:
-            print "quitting..."
-            sys.exit()
-    """
 
 
     def individual_videocatagory_analysis(self,data):
@@ -83,42 +53,6 @@ class DataStatistics():
                 lic=VideoCatagoryData.groupby('licensedContent')['video_category_id'].count()
                 print lic
 
-
-            """
-            columnNames = dataframe.columns.values
-            nl= dataframe.isnull().sum()
-            print "Number of rows containing null values =",nl
-            #print nl['description']
-            #print type(nl)
-            #print columnNames
-
-            print "Analysis for feature "
-            #dimension
-            dim=dataframe.groupby('dimension')['video_category_id'].count()
-
-            #definition
-            defi=dataframe.groupby('definition')['video_category_id'].count()
-
-            #caption
-            dataframe['caption'] = (dataframe['caption'])*1
-            #licensedContent
-            dataframe['licensedContent'] = (dataframe['licensedContent'])*1
-
-            #df1=dataframe[variable_names]
-            #print df1
-
-            new_dataframe  = dataframe[variable_names]
-            #print new_dataframe
-            description = new_dataframe.describe()
-            correlation = new_dataframe.corr(method='pearson', min_periods=1)
-            print "description of each feature - "
-            print description
-            print "correlation within features - "
-            print correlation
-            #print dataframe.groupby([columnNames[0]])['viewCount'].mean()
-            #for col in columnNames:
-            #    print
-            """
     def printCategories(self):
         """
             Print the list of features in the dataset 
